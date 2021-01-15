99803122ae54f6bd3a6fcc985759138cd1bd8bad
DjAlok123 <69834328+DjAlok123@users.noreply.github.com>
2021-01-15 18:23:37 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+# In this project I have made a own function to print, the name of the function is display()
+# I have also made a math module too!.
+# math module eg.
+# 1. the add() function
+# 2. the sub() function
+# 3. the multi() function
+# 4. the divide() function
+
+# to use the add function type add() and inside the brackets type your number eg.add(2,3) this will display 5 as output
+# same to all of the other function 
+# use the all math function inside the display function
+
+# this are the examples of the function.
+
+from math_cool import *
+
+display(add(5,5))
+display(sub(5,3))
+display(multi(5,5))
+display(int(divide(48,2)))
---
+++ math_cool.py
@@ -0,0 +1,22 @@
+# In this project I have made a own function to print, the name of the function is display()
+# I have also made a math module too!.
+# math module eg.
+# 1. the add() function
+# 2. the sub() function
+# 3. the multi() function
+# 4. the divide() function
+
+def add(num1, num2):
+	return num1 + num2
+
+def sub(num1, num2):
+	return num1 - num2
+
+def multi(num1, num2):
+	return num1 * num2
+
+def divide(num1, num2):
+	return num1 / num2
+
+def display(s):
+	return print(s)
